0f292c2e7d5c44c9d2badceafcff93c29c7aedc4
Anton Chepelev <chepelash@gmail.com>
2025-03-22 22:46:49 +0300

    fix: add autodocs

--- src/stories/OperationFull.stories.ts
+++ src/stories/OperationFull.stories.ts
@@ -6,6 +6,8 @@ import { createRandomOperation } from "./operationFunctions";
 const meta = {
   title: "Accounting/OperationFull",
   component: OperationFull,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: "fullscreen",
