fae29674ca2701e9ac66687dd54beecbd6a5d95b
Anton Chepelev <chepelash@gmail.com>
2025-04-06 20:59:52 +0300

    fix: add translation to changeButton

--- src/stories/headerWithContext/HeaderWithContext.tsx
+++ src/stories/headerWithContext/HeaderWithContext.tsx
@@ -9,19 +9,21 @@ import {
   ThemeProviderContext,
   ThemeProviderType,
 } from "../themeProvider/ThemeContext.ts";
+import { useTranslation } from "react-i18next";
 
 export const HeaderWithContext = () => {
   const { toggleLang, lang } =
     useContext<LangProviderType>(LangProviderContext);
   const { toggleTheme, theme } =
     useContext<ThemeProviderType>(ThemeProviderContext);
+  const { t } = useTranslation("buttons");
   return (
     <header
       className={`header ${theme === "light" ? "header-light" : "header-dark"}`}
     >
       <Logo />
       <button id="togleTheme" onClick={() => toggleTheme()}>
-        Change theme
+        {t("changeTheme")}
       </button>
       <button
         disabled={lang === "ru" ? true : false}
